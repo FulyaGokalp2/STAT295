3cfd1eb47ae7c8f5f4de37a9c44340b28b9d6b4c
FulyaGokalp2 <fulyagokalp@hotmail.com>
2024-02-19 15:10:07 +0300

    I added Index.R file.

---
+++ Index.R
@@ -0,0 +1,45 @@
+# STAT 295 
+# Revision 
+
+a <- 1+1
+vec <- 1:100
+vec
+class(vec)
+typeof(vec)
+# 1::100
+
+1+5
+3 * 5
+100/5
+
+1:6
+mydice <- 1:6
+mydice
+
+
+aaa <- 555
+AAA <- 666
+aaa
+ls()
+
+mydice - 1
+mydice * mydice
+
+mydice %*% mydice
+mydice %o% mydice
+
+dim(mydice) <- c(2,3)
+mydice
+dim(mydice) <- c(1,2,3)
+mydice
+
+m <- matrix(mydice, nrow = 2, byrow = TRUE)
+m
+
+myarray <- array(c(1:12), dim = c(2,2,3))
+myarray
+
+now <- Sys.time()
+now
+
+
